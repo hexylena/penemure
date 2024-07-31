197555c3fde948ec4f5a4451748d859188dce999
Helena Rasche <hexylena@galaxians.org>
2024-07-31 16:52:10 +0200

    hm

---
+++ cmd/assign.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	pmm "github.com/hexylena/pm/models"
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(assignCmd)
+
+}
+
+var assignCmd = &cobra.Command{
+	Use:   "assign [note id] [assignee]",
+	Short: "assign a note",
+	Args:  cobra.MinimumNArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		partial := pmm.PartialNoteId(args[0])
+		note_id, err := gn.GetIdByPartial(partial)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		note := gn.GetNoteById(note_id)
+		note.AddMetaTag("assignee", args[1])
+	},
+}
