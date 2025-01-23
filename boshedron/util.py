4af188105c9826323ca994802e6df4d1aa83a011
Helena Rasche <hexylena@galaxians.org>
2025-01-23 11:14:08 +0100

    smarter

--- boshedron/util.py
+++ boshedron/util.py
@@ -1,5 +1,14 @@
 import zoneinfo
 import datetime
+import os
+
+try:
+    LOCAL_ZONE = zoneinfo.ZoneInfo("localtime")
+except zoneinfo._common.ZoneInfoNotFoundError:
+    if 'TZ' in os.environ:
+        LOCAL_ZONE = zoneinfo.ZoneInfo(os.environ['TZ'])
+    else:
+        LOCAL_ZONE = zoneinfo.ZoneInfo('UTC')
 
 def local_now():
-    return datetime.datetime.now(zoneinfo.ZoneInfo("localtime"))
+    return datetime.datetime.now(tz=LOCAL_ZONE)
