b41ebddf2cde9dedf06f6f716f04f6364417f2b3
Helena Rasche <hexylena@galaxians.org>
2025-02-07 11:27:06 +0100

    allow initialising

---
+++ scripts/new-repo.py
@@ -0,0 +1,33 @@
+from boshedron.store import GitJsonFilesBackend
+from boshedron.main import *
+from boshedron.apps import *
+from boshedron.note import *
+from boshedron.tags import *
+import sqlglot
+import sys
+
+import argparse
+
+parser = argparse.ArgumentParser(
+                    prog='BOSHEDRON',
+                    description='Initialises a new repo')
+parser.add_argument('path', type=str)
+parser.add_argument('-n', '--name', type=str, required=True)
+parser.add_argument('-d', '--description', type=str, required=False, default='')
+args = parser.parse_args()
+
+
+be = GitJsonFilesBackend.new_meta(args.path, args.name, description=args.description)
+print(be)
+
+#
+# gb1 = GitJsonFilesBackend.discover('/home/user/projects/issues/')
+# gb2 = GitJsonFilesBackend.discover('./projects/alt')
+#
+# bos = Boshedron(backends=[gb1, gb2])
+# bos.load()
+#
+# n = Project(title="Testing")
+# bos.overlayengine.add(n)
+#
+# bos.save()
